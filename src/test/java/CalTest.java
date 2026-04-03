f511bb7e1d12bdbaa95d5a2156908f17ab37c135
nawonbin <nwb2247@gmail.com>
2026-04-03 16:49:31 +0900

    [FIX] fail test 주석처리

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -11,10 +11,10 @@ class CalTest {
         assertEquals(30, ret);
     }
 
-    @Test
-    void basicUnitTest_fail() {
-        Cal cal = new Cal();
-        int ret = cal.getSum(10, 20);
-        assertEquals(1, ret);
-    }
+//    @Test
+//    void basicUnitTest_fail() {
+//        Cal cal = new Cal();
+//        int ret = cal.getSum(10, 20);
+//        assertEquals(1, ret);
+//    }
 }
